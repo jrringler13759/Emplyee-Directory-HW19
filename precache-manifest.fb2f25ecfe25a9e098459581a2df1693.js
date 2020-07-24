488d39a03e20961534b2271370ec30c4ded8e450
Nikita <jrringler13759@gmail.com>
2020-07-24 15:03:51 -0400

    Updates

---
+++ precache-manifest.fb2f25ecfe25a9e098459581a2df1693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd3e76cce390cc9b29880ada441fa2b",
+    "url": "/Emplyee-Directory-HW19/index.html"
+  },
+  {
+    "revision": "7150b6cb70cf5f11c076",
+    "url": "/Emplyee-Directory-HW19/static/css/main.e127e706.chunk.css"
+  },
+  {
+    "revision": "dfbf0794a6de34f890d7",
+    "url": "/Emplyee-Directory-HW19/static/js/2.278e812f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Emplyee-Directory-HW19/static/js/2.278e812f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7150b6cb70cf5f11c076",
+    "url": "/Emplyee-Directory-HW19/static/js/main.f7b439cb.chunk.js"
+  },
+  {
+    "revision": "a5b0d83ec2501f58d757",
+    "url": "/Emplyee-Directory-HW19/static/js/runtime-main.de6c3525.js"
+  }
+]);
