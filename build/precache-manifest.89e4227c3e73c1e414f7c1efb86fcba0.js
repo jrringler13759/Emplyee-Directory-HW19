9d30e50b4ada3d24c5946cedbd5a036b1f9c8959
Nikita <jrringler13759@gmail.com>
2020-07-25 09:28:19 -0400

    added build folder

---
+++ build/precache-manifest.89e4227c3e73c1e414f7c1efb86fcba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0ab60e52268de0b33cf3c2923dbf52",
+    "url": "/Emplyee-Directory-HW19/index.html"
+  },
+  {
+    "revision": "8528adc314a654989e74",
+    "url": "/Emplyee-Directory-HW19/static/css/main.e127e706.chunk.css"
+  },
+  {
+    "revision": "dfbf0794a6de34f890d7",
+    "url": "/Emplyee-Directory-HW19/static/js/2.278e812f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Emplyee-Directory-HW19/static/js/2.278e812f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8528adc314a654989e74",
+    "url": "/Emplyee-Directory-HW19/static/js/main.f2309c15.chunk.js"
+  },
+  {
+    "revision": "a5b0d83ec2501f58d757",
+    "url": "/Emplyee-Directory-HW19/static/js/runtime-main.de6c3525.js"
+  }
+]);
